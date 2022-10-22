ca903aa2de5fe5afe930deead8c95129f645b89a
konradwala <konradwala1@gmail.com>
2022-10-22 22:10:35 +0200

    update
    Signed-off-by: konradwala <konradwala1@gmail.com>

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,19 +1,19 @@
 let turn = "x";
 let symbols = [["", "", ""], ["", "", ""], ["", "", ""]];
 
+
+
 const board = document.querySelector(".board");
-const tiles = Array.from(document.querySelectorAll(".tile")); //stworzenie tablicy z tile
+var tiles = Array.from(document.querySelectorAll(".tile")); //stworzenie tablicy z tile
 
 board.addEventListener("click", ({ target }) => { //jeśli kliknięto to wykonaj funkcje
   const classes = Array.from(target.classList); //tworzy tablice ze zwróconego DOMTokenList
-  if (classes.includes("tile") && classes.length !== 1) return; //to zapobiega obsłurzeniu już określonego pola
+  if (classes.includes("tile") && classes.length !== 1) return; //to zapobiega zmodyfikowaniu już określonego pola
 
   const idx = tiles.indexOf(target); //indeks wciśniętego pola (tilea)
   
-  console.log(Array.from(target.classList));
-  
   target.classList.add(`tile-${turn}`); //dodanie w div opisu tile x lub tile o
-  symbols[idx % 3][Math.floor(idx / 3)] = turn; //wypełnia tablice dwówymiarową xami i kółkami
+  symbols[idx % 3][Math.floor(idx / 3)] = turn; //wypełnia tablice dwówymiarową xem lub kółkiem
   turn = turn === "x" ? "o" : "x"; 
   // In an if statement it would look like: 
 // if (turn === 'X') {
@@ -25,34 +25,61 @@ board.addEventListener("click", ({ target }) => { //jeśli kliknięto to wykonaj
   displayTurn(turn);
 
   checkWin();
+  
+  reset();
 });
+
 
 
 
 function displayTurn(turn) 
 {
   var result = document.getElementById("turn");
-  result.textContent = `Tile ${turn}`;
- 
-
+  result.textContent = `Turn ${turn}`;
 }
 
 function checkWin() {
-  if(symbols[0][0]==symbols[0][1]&&symbols[0][1]==symbols[0][2])
-  alert("x or o is winner");
-  // 2. sprawdź czy któryś z graczy wygrał pojedynek - jeśli tak wyświetla komunikat (możesz użyć np. funkcji "alert(...)")
+  for (var i=0; i < 3; i++)
+  {
+    if(symbols[i][0]==symbols[i][1]&&symbols[i][1]==symbols[i][2]) {winnerAlert(symbols[i][0]);}
+    
+    if(symbols[0][i]==symbols[1][i]&&symbols[1][i]==symbols[2][i]) {winnerAlert(symbols[0][i]);}
+  }
+
+  if(symbols[0][0]==symbols[1][1]&&symbols[1][1]==symbols[2][2]) {winnerAlert(symbols[0][0]);}
+    
+  if(symbols[2][0]==symbols[1][1]&&symbols[1][1]==symbols[0][2]) {winnerAlert(symbols[2][0]);}
+    
+  function winnerAlert(xOrY)
+  {
+    if(xOrY!=="") 
+    {
+      alert("WINNER IS " + xOrY);
+    }
+  }
+  
 }
 
-// 3. dodaj listener pod przycisk z napisaem "reset" tak, aby po jego kliknięciu wywołać funkcję reset
-reset();
 function reset() {
   
-  const restetButton = document.querySelector(".reset");
-  restetButton.addEventListener("click", function(){
-    board.innerHTML = dupa;
-     
-    turn = "x";
-    symbols = [["", "", ""], ["", "", ""], ["", "", ""]];
+  const resetButton = document.querySelector(".reset");
+  resetButton.addEventListener("click", function(){
+  
+
+  for(var i=0; i<9; i++)
+  { 
+    var oldChild = document.getElementById("tile");
+  var newChild = document.createElement("div");
+  newChild.setAttribute("class", "tile");
+  newChild.setAttribute("id","tile");
+    oldChild.remove();
+    board.append(newChild);
+  }
+
+
+   symbols = [["", "", ""], ["", "", ""], ["", "", ""]];
+   turn = "x";
+   tiles = Array.from(document.querySelectorAll(".tile"));
   });
   
 }
