0b961e42cfe38e2d573a566be67e63c76697851a
konradwala <konradwala1@gmail.com>
2022-10-23 19:14:02 +0200

    update
    Signed-off-by: konradwala <konradwala1@gmail.com>

--- gotta-catch-em-all/index.js
+++ gotta-catch-em-all/index.js
@@ -5,14 +5,16 @@
 */
 
 // tutaj złapiemy sekcję, do której będziemy dodawać pokemony
-const pokemonsContainer = document.querySelector(".pokemons");
+var pokemonsContainer = document.querySelector(".pokemons");
 
 function renderPokemons(pokemons) {
-  for(var i = 0; i < 151; i++)
+  pokemonsContainer.remove();
+  for(var i = 0; i < pokemons.length; i++)
   {
     var newPokemon = document.createElement("div");
+    newPokemon.setAttribute("class",pokemons[i].types);
+    newPokemon.setAttribute("id",pokemons[i].id)
     newPokemon.innerHTML = pokemons[i].name;
-    
     pokemonsContainer.append(newPokemon);
   }
 }
@@ -27,17 +29,50 @@ renderPokemons(pokemons);
   - filtrowanie po nazwie (wpisany fragment zawiera się w nazwie pokemona)
 */
 
+
+
 function filterPokemons(pokemons) {
+  var filters = Array.from(document.querySelectorAll("input"));
+  //console.log(filters);
+
+  var returnArray = [];
+
+  for(var x = 0; x < pokemons.length; x++)
+  {
+    for(var i = 0 ; i <16; i++)
+    {
+      if(filters[i].checked==true)
+      {
+        if(pokemons[x].types==filters[i].id)
+        {
+          returnArray.push(pokemons[x]);
+          break;
+        }
+      }
+      //console.log(filters[i].checked + filters[i].id);
+    }
+  }
+  console.log(returnArray);
+  return returnArray;
+
+
+
+
+
   // uzupełnij tutaj
   // zwróć odfiltrowaną tablicę pokemonów
 }
+
+
+
+
 
 const form = document.querySelector("form");
 
 function submitForm(event) {
   event.preventDefault();
   // następnie wykonaj uzupełnioną metodę z tablicą pokemons, aby sprawdzić czy wszystko działa
-  // renderPokemons(filterPokemons(pokemons));
+  renderPokemons(filterPokemons(pokemons));
 }
 
 form.addEventListener("submit", submitForm);
